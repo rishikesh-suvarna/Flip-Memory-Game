bb98c4900f72a200b8cd72bd32e653b57d50fffb
Rishikesh Suvarna <rishikeshsuvarna@gmail.com>
2024-08-29 01:38:09 +0100

    feat: Completd the game functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Card from '@/components/Card'
-import React, { useLayoutEffect } from 'react'
+import React, { useCallback } from 'react'
 
 type Action = { type: 'SET_CARDS', payload: any[] } | { type: 'SET_TURNS', payload: number } | { type: 'SET_CHOICE_ONE', payload: any } | { type: 'SET_CHOICE_TWO', payload: any } | { type: 'DISABLED', payload: boolean } | { type: 'RESET' };
 type State = {
@@ -69,7 +69,7 @@ const reducer = (state: typeof INITAL_STATE, action: Action) => {
     case 'SET_CARDS':
       return { ...state, cards: action.payload };
     case 'SET_TURNS':
-      return { ...state, turns: action.payload };
+      return { ...state, turns: action.payload === 0 ? 0 : state.turns + 1 };
     case 'SET_CHOICE_ONE':
       return { ...state, choiceOne: action.payload };
     case 'SET_CHOICE_TWO':
@@ -87,6 +87,7 @@ const Home = () => {
   const [state, dispatch] = React.useReducer(reducer, INITAL_STATE);
 
   const shuffleCards = () => {
+    console.log(`Shuffling Cards`);
     const shuffledCards = [...CARDS, ...CARDS]
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
@@ -94,36 +95,77 @@ const Home = () => {
     setCards(shuffledCards);
     setChoiceOne(null);
     setChoiceTwo(null);
-    setTurns();
+    setTurns(0);
   };
 
   const setCards = (cards: any[]) => dispatch({ type: 'SET_CARDS', payload: cards });
-  const setTurns = () => dispatch({ type: 'SET_TURNS', payload: state.turns + 1 });
-  const setChoiceOne = (choice: any) => dispatch({ type: 'SET_CHOICE_ONE', payload: choice });
-  const setChoiceTwo = (choice: any) => dispatch({ type: 'SET_CHOICE_TWO', payload: choice });
-  const setDisabled = (disabled: boolean) => dispatch({ type: 'DISABLED', payload: disabled });
+  const setTurns = React.useCallback((turns: number) => dispatch({ type: 'SET_TURNS', payload: turns }), []);
+  const setChoiceOne = React.useCallback((choice: any) => dispatch({ type: 'SET_CHOICE_ONE', payload: choice }), []);
+  const setChoiceTwo = React.useCallback((choice: any) => dispatch({ type: 'SET_CHOICE_TWO', payload: choice }), []);
+  const setDisabled = React.useCallback((disabled: boolean) => dispatch({ type: 'DISABLED', payload: disabled }), []);
   const reset = () => dispatch({ type: 'RESET' });
 
-  useLayoutEffect(() => {
+  const handleChoice = (card: any) => {
+    console.log(card)
+    if (!state.choiceOne) {
+      setChoiceOne(card);
+    } else {
+      setChoiceTwo(card);
+    }
+  };
+
+  const resetTurn = React.useCallback(() => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(1);
+    setDisabled(false);
+  }, [setChoiceOne, setChoiceTwo, setTurns, setDisabled]);
+
+  React.useEffect(() => {
+    console.log(`Running`);
+    if (state.choiceOne && state.choiceTwo) {
+      if (state.choiceOne.name === state.choiceTwo.name) {
+        const newCards = state.cards.map((card) => {
+          if (card.name === state.choiceOne.name) {
+            return { ...card, matched: true };
+          }
+          return card;
+        });
+
+        setCards(newCards);
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [state.choiceOne, state.choiceTwo]);
+  
+  React.useEffect(() => {
     shuffleCards();
   }, []);
 
   return (
     <main>
       <section className='py-10'>
-        <div className='container mx-auto'>
-          <h1 className='text-4xl font-light text-center mb-8'>Flip Game</h1>
-          <div className="grid grid-cols-4 gap-4">
-            {
-              state.cards.map((card, index) => (
-                <Card
-                  key={index}
-                  matched={card.matched}
-                  front={card.img}
-                />
-              ))
-            }
+        <div className='max-w-[800px] mx-auto'>
+          <div className="flex items-center justify-between mb-8">
+            <h1 className='text-5xl font-light text-center'>Flip Game</h1>
+            <p className='text-center font-mono text-normal'>Turns: {state.turns}</p>
           </div>
+            <div className="grid grid-cols-4 gap-3">
+              {
+                state.cards.map((card) => (
+                  <Card
+                    card={card}
+                    key={card.id}
+                    flipped={state.choiceOne === card || state.choiceTwo === card || card.matched}
+                    front={card.img}
+                    disabled={state.disabled}
+                    handleChoice={handleChoice}
+                  />
+                ))
+              }
+            </div>
         </div>
       </section>
     </main>
--- components/Card.tsx
+++ components/Card.tsx
@@ -4,23 +4,30 @@ import Image from 'next/image'
 import React from 'react'
 
 const Card = ({
+    card,
     front,
-    matched
+    flipped,
+    disabled,
+    handleChoice
 }: {
+    card: any
     front: string
-    matched: boolean
+    flipped: boolean,
+    disabled: boolean,
+    handleChoice: (card: any) => void
 }) => {
 
-    const [flipped, setFlipped] = React.useState(false)
 
     const handleCardFlip = () => {
-      setFlipped(!flipped)
+        if (!disabled) {
+            handleChoice(card)
+        }
     }
 
     return (
-        <div className={`card ${flipped || matched ? 'flipped' : ''}`}>
+        <div className={`card ${flipped ? 'flipped' : ''}`}>
             <div className="front w-full h-full p-2 grid place-items-center">
-                <Image src={front} alt="card" width={60} height={60} objectFit='cover'  />
+                <Image src={front} alt="card" width={60} height={60} style={{ objectFit: 'cover' }} />
             </div>
             <div className="back bg-gray-300 w-full h-full p-2" onClick={handleCardFlip}></div>
         </div>
